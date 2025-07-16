946dc8fca97c26f223d91a8f0e389db441851688
reo0306 <reo19840306@gmail.com>
2025-07-16 11:53:04 +0900

    add one

---
+++ typescript-type/index.ts
@@ -0,0 +1,7 @@
+let hasValue: boolean = false;
+let count: number = 10;
+let float: number = 2.14;
+let negative: number = -0.12;
+let single: string = 'hello';
+let double: string = "hello";
+let back: string = `hello`;
