2fbcc5f7922d94da9f0fc6dda8c8f242276d8dae
reo0306 <reo19840306@gmail.com>
2025-07-20 23:15:51 +0900

    update class.js

--- src/class.ts
+++ src/class.ts
@@ -1,8 +1,47 @@
 class Person {
-    name: string;
-    constructor(initName: string) {
-        this.name = initName;
+    static species = 'Homo sapiens';
+
+    static isAdult(age: number) {
+        if (age > 17) return true;
+
+        return false;
+    }
+
+    constructor(readonly name: string, protected age: number) {
+    }
+
+    incrementAge() {
+        this.age += 1;
+    }
+
+    gretting(this: Person) {
+        console.log(`Hello! My name is ${this.name}. I am ${this.age} years old.`);
     }
 }
-const quill = new Person("Quill");
-console.log(quill);
+
+class Teacher extends Person {
+    get subject(): string {
+        if (!this._subject) {
+            throw new Error('There is no subject.')
+        }
+        return 'Music';
+    }
+
+    set subject(value) {
+        if (!this._subject) {
+            throw new Error('There is no subject.')
+        }
+        this._subject = value;
+    }
+
+    constructor(name: string, age: number, private _subject: string) {
+        super(name, age);
+        //super.gretting();
+    }
+    gretting(this: Person) {
+        console.log(`Hello! My name is ${this.name}. I am ${this.age} years old. I tearch ${this.subject}`);
+    }
+}
+const teacher = new Teacher('Quill', 38, 'Math');
+console.log(teacher.subject);
+teacher.gretting();
