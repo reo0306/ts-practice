bfdb32df78db4dea9547b3f8c4f06a2cd60fe324
reo0306 <reo19840306@gmail.com>
2025-07-21 22:24:27 +0900

    add interface

---
+++ src/interface.ts
@@ -0,0 +1,13 @@
+interface Human {
+    name: string;
+    age: number;
+    greeting(message: string): void;
+}
+
+const human: Human = {
+    name: 'Quill',
+    age: 38,
+    greeting(message: string): void {
+        console.log(message);
+    }
+}
