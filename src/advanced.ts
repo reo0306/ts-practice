ee22839b0c218da1ecc2a648ac1a11d942c2b3f8
reo0306 <reo19840306@gmail.com>
2025-07-22 10:44:56 +0900

    add advanced.ts

---
+++ src/advanced.ts
@@ -0,0 +1,70 @@
+interface Enginner {
+    name: string,
+    role: string,
+}
+
+interface Blogger {
+    name: string,
+    follower: number,
+}
+
+//type EnginnerBlogger = Enginner & Blogger;
+interface EnginnerBlogger extends Enginner, Blogger {}
+
+const quill: EnginnerBlogger = {
+    name: 'Quill',
+    role: 'front-end',
+    follower: 1000,
+}
+
+type NumberBoolean = number | boolean;
+type StringNumber = string | number;
+type Mix = NumberBoolean & StringNumber;
+
+function toUpperCase(x: string | number) {
+    if (typeof x === 'string') {
+        return x.toUpperCase();
+    }
+    return '';
+}
+
+type NomadWorker = Enginner | Blogger;
+function describeProfile(nomadWorker: NomadWorker) {
+    console.log(nomadWorker.name);
+    if ('role' in nomadWorker) {
+        console.log(nomadWorker.role)
+    }
+    if ('follower' in nomadWorker) {
+        console.log(nomadWorker.follower);
+    }
+}
+
+class Dog {
+    kind: 'dog' = 'dog'
+    speak() {
+        console.log('bow-wow');
+    }
+}
+
+class Bird {
+    kind: 'bird' = 'bird' // tag付きユニオン
+    speak() {
+        console.log('tweet-tweet')
+    }
+    fly() {
+        console.log('flutter')
+    }
+}
+
+type Pet = Dog | Bird;
+function havePet(pet: Pet) {
+    pet.speak();
+    switch (pet.kind) {
+        case 'bird':
+            pet.fly();
+    }
+    if (pet instanceof Bird) {
+        pet.fly();
+    }
+}
+havePet(new Bird());
