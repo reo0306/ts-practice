6d3d2cd0363a627480b222549b9c17e02cb55a92
reo0306 <reo19840306@gmail.com>
2025-07-22 09:59:47 +0900

    update interface four

--- src/interface.ts
+++ src/interface.ts
@@ -1,5 +1,19 @@
+//type addFunc = (num1: number, num2: number) => number;
+interface addFunc {
+    (num1: number, num2: number): number;
+}
+
+let addFunc: addFunc;
+addFunc = (n1: number, n2: number) => {
+    return n1 * n2;
+}
 interface Nameable {
-    name: string;
+    name?: string;
+    nickName?: string;
+}
+const nameable: Nameable = {
+    name: 'Quill',
+    nickName: 'Quilla',
 }
 
 interface Human extends Nameable {
@@ -8,9 +22,10 @@ interface Human extends Nameable {
 }
 
 class Developer implements Human {
-    constructor(public name: string, public age: number, public experience) {}
+    constructor(public age: number, public experience, public name?: string) {
+    }
 
-    greeting(message: string): void {
+    greeting(message: string = 'hello'): void {
         console.log(message);
     }
 }
@@ -22,4 +37,10 @@ const tmpDeveloper = {
         console.log(message);
     }
 }
-let user: Human = tmpDeveloper;
+//let user: Human = tmpDeveloper;
+
+const user: Human = new Developer(38, 3, 'Quill');
+if (user.name) {
+    user.name.toUpperCase();
+}
+console.log(user.name);
