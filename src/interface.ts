52f498642866c186fb9b8b5cce061f0f1f5ccfbb
reo0306 <reo19840306@gmail.com>
2025-07-22 09:34:54 +0900

    update interface three

--- src/interface.ts
+++ src/interface.ts
@@ -1,13 +1,25 @@
-interface Human {
+interface Nameable {
     name: string;
+}
+
+interface Human extends Nameable {
     age: number;
     greeting(message: string): void;
 }
 
 class Developer implements Human {
-    constructor(public name: string, public age: number) {}
+    constructor(public name: string, public age: number, public experience) {}
 
     greeting(message: string): void {
-        console.log(`Hello!`);
+        console.log(message);
     }
 }
+
+const tmpDeveloper = {
+    name: 'Quill',
+    age: 38,
+    greeting(message: string) {
+        console.log(message);
+    }
+}
+let user: Human = tmpDeveloper;
