6870b60facb44d8bfec555102c71bc1de61eafd0
reo0306 <reo19840306@gmail.com>
2025-07-24 07:48:51 +0900

    update generics

--- src/generics.ts
+++ src/generics.ts
@@ -1,7 +1,73 @@
 // 型を引数として渡せる,制約をつける
-function copy<T extends { name: string }>(value: T): T {
+function copy<T extends { name: string }, U extends keyof T>(value: T, key: U): T {
+    value[key];
     return value;
 }
-console.log(copy({ name: 'Quill'}));
+console.log(copy({ name: 'Quill', age: 38}, 'name'));
 
-const arrowFunc = <T>(num: T) => num;
+// キーからユニオン型を作る
+type K = keyof { name: string, age: number};
+
+class LightDatabase<T extends string | number | boolean> {
+    private data: T[] = [];
+    add(item: T) {
+        this.data.push(item);
+    }
+    remove(item: T) {
+        this.data.splice(this.data.indexOf(item), 1);
+    }
+    get() {
+        return this.data;
+    }
+}
+
+const stringLightDatabase = new LightDatabase<string>();
+stringLightDatabase.add('Apple');
+stringLightDatabase.add('Banana');
+stringLightDatabase.add('Grape');
+stringLightDatabase.remove('Banana');
+console.log(stringLightDatabase.get());
+
+interface TmpDatabase<T> {
+    id: number,
+    data: T[],
+}
+
+const tmpDatabase: TmpDatabase<number> = {
+    id: 3,
+    data: [32]
+}
+
+type TmpDatabase2<T> {
+    id: number,
+    data: T[],
+}
+
+// ユーティリティタイプ
+interface Todo {
+    title: string,
+    text: string,
+}
+// 全部オプションにする
+type Todoable = Partial<Todo>
+// 全部readonlyにする
+type ReadTodo= Readonly<Todo>
+
+const fetchData: Promise<string> = new Promise(resolve => {
+    setTimeout(() => {
+        resolve('hello');
+    }, 3000)
+});
+
+fetchData.then(data => {
+    data.toUpperCase();
+});
+
+// 配列の型
+const vegetables: Array<string> = ['Tomato', 'Broccoli', 'Asparagus'];
+
+interface ResponseData<T extends { message: string} = any> {
+    data: T,
+    status: number,
+}
+let tmp2: ResponseData;
