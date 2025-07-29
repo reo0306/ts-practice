2263ddfbb25974eb413598d562f14d91a02c0301
reo0306 <reo19840306@gmail.com>
2025-07-29 17:59:16 +0900

    update typescript-node two

--- typescript-node/src/app.ts
+++ typescript-node/src/app.ts
@@ -1,4 +1,30 @@
-//import * as express from "express";
-import express = require('express');
+import express, { Request, Response, NextFunction } from 'express';
+
+interface MessageRequest extends Request {
+    body: {
+        message: string,
+    }
+}
 const app = express();
+app.use(express.json());
+
+app.use('/', (req: Request, res: Response, next: NextFunction) => {
+    console.log('hello');
+    throw new Error()
+    next();
+});
+
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+    res.send('<h1>Hello</h1>')
+});
+
+app.post('/', (req: MessageRequest, res: Response, next: NextFunction) => {
+    console.log(req.body);
+    res.send(`<h1>${req.body.message}</h1>`)
+});
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    next();
+});
+
 app.listen(3000);
